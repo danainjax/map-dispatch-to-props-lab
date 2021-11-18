0d32beef24d4ca6686e8115a6fcef539f1e4a263
danainjax <danainjax@gmail.com>
2021-11-18 13:58:31 -0500

    commit tree 29a3f1a7867c967be3709490b2725c1317cc692e

--- src/components/RestaurantInput.js
+++ src/components/RestaurantInput.js
@@ -1,54 +1,58 @@
-import React, { Component } from 'react';
-import { addRestaurant } from '../actions/restaurants';
-import { connect } from 'react-redux';
+import React, { Component } from 'react'
+import { addRestaurant } from '../actions/restaurants'
+import { connect } from 'react-redux'
 
 export class RestaurantInput extends Component {
-
   state = {
     name: '',
-    location: ''
+    location: '',
   }
 
-  handleOnNameChange = event => {
+  handleOnNameChange = (event) => {
     this.setState({
-      name: event.target.value
-    });
+      name: event.target.value,
+    })
   }
 
-  handleOnLocationChange = event => {
+  handleOnLocationChange = (event) => {
     this.setState({
-      location: event.target.value
-    });
+      location: event.target.value,
+    })
   }
 
-  handleOnSubmit = event => {
-    event.preventDefault();
-    // add missing code
+  // mapDispatchToProps = (dispatch) => {
+  //   console.log('dispatch')
+  // }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault()
+    this.props.addRestaurant(this.state)
   }
 
   render() {
-    return(
+    return (
       <form onSubmit={(event) => this.handleOnSubmit(event)}>
         <p>
           <input
-            type="text"
+            type='text'
             onChange={(event) => this.handleOnNameChange(event)}
-            id="name"
-            placeholder="restaurant name" />
+            id='name'
+            placeholder='restaurant name'
+          />
         </p>
         <p>
           <input
-            type="text"
+            type='text'
             onChange={(event) => this.handleOnLocationChange(event)}
-            id="location"
-            placeholder="location" />
+            id='location'
+            placeholder='location'
+          />
         </p>
-        <input type="submit" />
+        <input type='submit' />
       </form>
-    );
+    )
   }
-};
-
+}
 
 //connect this component by wrapping RestaurantInput below
-export default RestaurantInput
+export default connect(null, { addRestaurant })(RestaurantInput)
